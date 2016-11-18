3c655b069df9b714735f3fb0308d29b865cf10d2
Siim Aus <siim.aus@tvesi.ee>
2016-11-18 17:32:40 +0200

    Had some success with extension methods
    intermediate work

--- Extenstions/EntityVersioningExtensions.cs
+++ Extenstions/EntityVersioningExtensions.cs
@@ -15,10 +15,24 @@ using System.Linq;
 using ASTV.Models.Generic;
 
 namespace ASTV.Extenstions {
+    public class VersionInfo {
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public Boolean IsCurrent { get; set; }
+        public Boolean IsDeleted { get; set; }
+        public int Version { get; set; }
+
+    }
     public static class EntityVersioningExtensions {
+
+        public static readonly Dictionary<string, Type> VersionProperties = new Dictionary<string, Type>(){
+            { "ValidFrom", typeof(System.DateTime)}
+        };         
 
         public static void AddVersioningAttributes(this DbContext context, ModelBuilder modelBuilder) {
             // Versioning information. Need to create index on previous key and version number?
+            // TODO: save original key to context cache?
+             
             foreach (var entityType in modelBuilder.Model.GetEntityTypes()
                 .Where(e => typeof(IEntityVersioning).IsAssignableFrom(e.ClrType)))
             {
@@ -76,6 +90,143 @@ namespace ASTV.Extenstions {
         internal static readonly MethodInfo PropertyMethod
             = typeof(EF).GetTypeInfo().GetDeclaredMethod(nameof(Property));
         
+        public static string ListObject(object O) {
+            string info = "";
+            Type t = O.GetType();
+            info += "Type: "+t.FullName;
+            info += "Members:\n";
+            foreach( var s in t.GetProperties()) {
+                info += s.Name + ":"; //+s.GetType().FullName;
+                if (s.GetType().FullName == "System.Reflection.RuntimePropertyInfo") {
+                    
+                    info += ":"+s.PropertyType.FullName;
+                    var oo = s.GetValue(O, null);
+                    if (oo == null) {
+                        info += "= (null)";
+                    } else {
+                        info += "= "+oo.ToString();
+                    }
+                    
+                }
+                info += "\n";
+            }
+
+            return info;
+        }
+
+
+
+        public static IKey VK<TEntity>(this DbSet<TEntity> set) where TEntity : class
+        {
+            var context =  set.GetService<IDbContextServices>().CurrentContext.Context; 
+            var entityType = context.Model.FindEntityType(typeof(TEntity));
+            var pk = entityType.FindPrimaryKey();
+            var keys = entityType.GetKeys();
+            foreach(var key in keys) {
+                if (key.Properties.Where(m => m.Name == "Version").Count() > 0) {
+                    Console.WriteLine("AK FOUND: '{0}': {1}", key.SqlServer().Name, key.Properties.Select(m => m.Name).ToArray().Join(", "));
+                    return key;
+                }                
+            }
+            return null;
+        }
+
+        public static void BuildVersionExpression<TEntity>(this DbSet<TEntity> set, TEntity entity) where TEntity : class
+        {   
+            var vk = set.VK();
+            if (vk == null) return;            
+            // enumerate values of key properties            
+            // I need to get latest version no from db by AK and increase it by 1. 
+            // I also need to mark latest as not current, current as current and update dates
+
+
+        }
+
+        /// <summary>
+        /// Returns latest versioninfo for entity 
+        /// </summary>
+        public static VersionInfo Latest<TEntity>(this IQueryable<TEntity> source,  TEntity entity) where TEntity : class
+        {            
+            if (entity == null ) return null;
+            if (typeof(DbSet<TEntity>).IsAssignableFrom(source.GetType())) {
+                var set = (DbSet<TEntity>)source;  
+                var ak = set.VK();
+                if (ak == null) { return null; } // no version keys
+                string kv = "";
+                foreach(var property in ak.Properties) {
+                    if (property.Name != "Version") {
+                        // get value of entity of given name.
+                        var oo = entity.GetType().GetTypeInfo().GetProperties().Where(p => p.Name == property.Name).Select(p => p.GetValue(entity,null)).FirstOrDefault();
+                        kv  += property.Name+ "=";
+                        //var oo = s.GetValue(O, null);
+                        if (oo == null) {
+                            kv += "(null)";
+                        } else {
+                            kv += oo.ToString();
+                        }
+                        kv += " ";
+                    }                    
+                }
+                kv = "Search Key: '"+kv+"'";
+                Console.WriteLine(kv);
+            }
+            return null;
+        }
+
+        public static IQueryable<TEntity> IsCurrent<TEntity>(this IQueryable<TEntity> source,  TEntity entity) where TEntity : class
+        {
+            if (typeof(DbSet<TEntity>).IsAssignableFrom(source.GetType())) {
+                var set = (DbSet<TEntity>)source;                                
+                Console.WriteLine("Well, well, Yes yes, it is!");
+                set.VK();
+                var parameter = Expression.Parameter(typeof(TEntity), "x");
+                var expression = Expression.Lambda(
+                    //Expression.And(  
+                        Expression.Equal(
+                            Expression.Call(
+                                PropertyMethod.MakeGenericMethod(typeof(Boolean) )
+                                , parameter
+                                , Expression.Constant("IsCurrent", typeof(string))
+                            )
+                            ,
+                            Expression.Constant(true)),
+                            /*,
+                        Expression.Equal(
+                            Expression.Property(parameter, "Version"),
+                            Expression.Constant(1))
+                            ),*/
+                        parameter) as Expression<Func<TEntity, bool>>;
+                return source.Where(expression);
+            }        
+            return source;
+        }
+        public static void PK<TEntity>(this DbSet<TEntity> set, params object[] keyValues) where TEntity : class
+        {
+            var context =  set.GetService<IDbContextServices>().CurrentContext.Context; 
+            var entityType = context.Model.FindEntityType(typeof(TEntity));
+            var pk = entityType.FindPrimaryKey();
+            //entityType.GetProperties()
+            //var xxx = entityType.GetProperties().ToList().AsReadOnly();
+
+            
+            //var ak = entityType.FindKey(xxx);
+            var keys = entityType.GetKeys();
+            if (pk != null) {
+                Console.WriteLine("PK: {0}", pk.Properties.Select(m => m.Name).ToArray().Join(", "));
+            }
+
+            var ak = entityType.FindKey(entityType.GetProperties().Where(m => m.Name == "Version" ).ToList().AsReadOnly());
+
+            if (ak != null) {
+                Console.WriteLine("AK: '{0}': {1}", ak.SqlServer().Name, ak.Properties.Select(m => m.Name).ToArray().Join(", "));
+            }
+
+            foreach(var key in keys) {
+                Console.WriteLine("key: '{0}': {1}", key.SqlServer().Name, key.Properties.Select(m => m.Name).ToArray().Join(", "));
+            }
+
+        }
+
         public static void AddVersion<TEntity>(this DbSet<TEntity> set, params object[] keyValues) where TEntity : class
         {
             // get latest information from database
@@ -114,12 +265,64 @@ namespace ASTV.Extenstions {
             Console.WriteLine("Linq Expression: {0}",  expression.ToString());
             var cdd = expression.Compile();
             //set.AsNoTracking().Where(expression).Max();
-            IList<TEntity> x =set.Where(cdd).ToList();
-             Console.WriteLine("CNN is : {0}", x.Count);
-            foreach(var nn  in set.Where(expression).ToList()) {
+//            IList<TEntity> x =set.Where(cdd).ToList();
+//             Console.WriteLine("CNN is : {0}", x.Count);
+            foreach(var nn  in set.AsNoTracking().Where(expression).ToList()) {
                 Console.WriteLine("NN is : {0}", nn.ToString());
+                Console.WriteLine("NN info: \n{0}", ListObject(nn));
             }
             //set.AsNoTracking().Where(p => p.Equals())
         }
+
+
+
+        
+        public static void GetLatestVersion<TEntity>(this DbSet<TEntity> set, params object[] keyValues) where TEntity : class
+        {
+            // get latest information from database
+            MethodInfo PropertyMethodx
+            = typeof(EF).GetTypeInfo().GetDeclaredMethod(nameof(Property));
+            var context =  set.GetService<IDbContextServices>().CurrentContext.Context; 
+            var entityType = context.Model.FindEntityType(typeof(TEntity));
+            var keys = entityType.GetKeys();
+            var entries = context.ChangeTracker.Entries<TEntity>();
+            var parameter = Expression.Parameter(typeof(TEntity), "x");
+            //EF.Property<Boolean>(parameter, "IsCurrent");
+        
+            /*
+            Expression.Call(
+                            EF.PropertyMethod.MakeGenericMethod(property.ClrType),
+                            entityParameter,
+                            Expression.Constant(property.Name, typeof(string))
+            */
+            // should extract keys from key which has "Version" defined and get its latest value
+             var expression = Expression.Lambda(
+                    //Expression.And(  
+                        Expression.Equal(
+                            Expression.Call(
+                                PropertyMethod.MakeGenericMethod(typeof(Boolean) )
+                                , parameter
+                                , Expression.Constant("IsCurrent", typeof(string))
+                            )
+                            ,
+                            Expression.Constant(true)),
+                            /*,
+                        Expression.Equal(
+                            Expression.Property(parameter, "Version"),
+                            Expression.Constant(1))
+                            ),*/
+                        parameter) as Expression<Func<TEntity, bool>>;
+            Console.WriteLine("Linq Expression: {0}",  expression.ToString());
+            var cdd = expression.Compile();
+            //set.AsNoTracking().Where(expression).Max();
+//            IList<TEntity> x =set.Where(cdd).ToList();
+//             Console.WriteLine("CNN is : {0}", x.Count);
+            foreach(var nn  in set.Where(expression).ToList()) {
+                Console.WriteLine("NN is : {0}", nn.ToString());
+                
+            }
+            //set.AsNoTracking().Where(p => p.Equals())
+        }
+
     }
 }
--- Repositories/Implementation/EntityBaseRepository.cs
+++ Repositories/Implementation/EntityBaseRepository.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 
+using ASTV.Extenstions;
+
 namespace ASTV.Services {
     /// <summary>
     /// Base repository with modification capabilities
@@ -21,10 +23,46 @@ namespace ASTV.Services {
             //this._context = context;
         }
 
+
+        private IQueryable<T> ApplyVersionFilter(IQueryable<T> source ) {
+            return source.Where(e => EF.Property<Boolean>(e, "IsCurrent") == true);
+        } 
+
         public virtual void Add(T entity)
         {
+
+            Console.WriteLine("==========================================================\n");
+            Console.WriteLine("Entity to be saved: \n{0}", EntityVersioningExtensions.ListObject(entity));
+            Console.WriteLine("==========================================================\n");
+            //VersionInfo x = new VersionInfo();
+            IQueryable<T> qq = _context.Set<T>();
+            Console.WriteLine("CCOUNT IS:{0}", qq.IsCurrent(entity).Count());
+
+            VersionInfo ll = qq.Latest(entity);
+
+            foreach(var xx in _context.Set<T>().AsNoTracking().Where(
+                e => EF.Property<Boolean>(e, "IsCurrent") == true).Select( m => new VersionInfo { IsCurrent = EF.Property<Boolean>(m, "IsCurrent") } )) {
+                Console.WriteLine(xx.ToString());
+                
+                Console.WriteLine("XX info: \n{0}", EntityVersioningExtensions.ListObject(xx));
+            }
+            _context.Set<T>().PK("");
+            var k = _context.Set<T>().VK();
+            if (k != null) {
+                Console.WriteLine("Versioning key found: ");
+            }
+
+
             EntityEntry dbEntityEntry = _context.Entry<T>(entity);            
-            _context.Set<T>().Add(entity);                          
+            _context.Set<T>().Add(entity);                   
+                        
+            Console.WriteLine("Entity added");
+            IList<T> o = _context.Set<T>().ToList();
+            int i = 0;
+            foreach(var x in o) {
+                i++;
+                Console.WriteLine("Line: {0}:{1}", i, x.ToString());
+            }
         }
         public virtual void Update(T entity) {
             EntityEntry dbEntityEntry = _context.Entry<T>(entity);
