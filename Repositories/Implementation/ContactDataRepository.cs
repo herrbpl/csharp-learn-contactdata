e94819fe5d65d9bbc9b2ca663153d14cf9dfa57f
Siim Aus <siim.aus@tvesi.ee>
2016-11-29 16:10:54 +0200

    Added ContactDataRepository
    Basic setup

--- Repositories/Implementation/ContactDataRepository.cs
+++ Repositories/Implementation/ContactDataRepository.cs
@@ -6,12 +6,14 @@ using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 
 using ASTV.Models.Employee;
+using ASTV.Models.Generic;
 
 namespace ASTV.Services {
-    public class ContactDataRepository<TContext>: EntityBaseRepositoryReadOnly<ContactData, TContext>                
-        where TContext : ContactDataContext       
+
+    public interface IContactDataRepository: IEntityBaseRepository<ContactData, ContactDataContext> {}
+    public class ContactDataRepository: EntityBaseRepository<ContactData, ContactDataContext>, IContactDataRepository                               
     {
-        public ContactDataRepository(TContext context) : base(context) {            
+        public ContactDataRepository(ContactDataContext context) : base(context) {            
         }
         // add - if exists, update, else add
         // update - if exist, create old version
@@ -19,7 +21,31 @@ namespace ASTV.Services {
 
         // Get all latest versions
         public override IEnumerable<ContactData> GetAll() {
-            return null;
+             ContactData cd = new ContactData();
+                // should it be here??
+                var lr = new LanguageRepository(this._context);
+                var er = new EducationRepository(this._context);
+
+                Language ll = lr.GetAll().Where(l => l.Code=="EE").FirstOrDefault();
+                EducationLevel el = er.GetAll().Where(l => l.Code=="ah").FirstOrDefault();
+
+                Education edu = new Education();
+                edu.SchoolName = "Uus kool";
+                edu.YearCompleted = 2015;
+                edu.NameOfDegree = "uu";
+                edu.Level = el;
+
+
+                cd.FirstName = "Siim";
+                cd.LastName = "Aus";
+                cd.JobTitle = "IT Director";
+                cd.ContactLanguage = ll;
+                cd.Education.Add(edu);
+
+                cd.EmployeeId = "0203";
+            IList<ContactData> a = new List<ContactData>();
+            a.Add(cd);
+            return a;
         }
     }
 }    
