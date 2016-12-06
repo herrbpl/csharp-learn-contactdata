1786f0c74b9e7c59117bb6c614ade73e9988ecc7
Siim Aus <siim.aus@tvesi.ee>
2016-12-06 16:20:39 +0200

    Cleanup

--- Contexts/LDAPContext.cs
+++ Contexts/LDAPContext.cs
@@ -20,7 +20,7 @@ namespace ASTV.Services {
 
     public interface ILDAPContext<TEntity> {
         IList<TEntity> Search(int scope, string filter );                
-        IList<TEntity> Search(int scope, string filter, IDictionary<string, KeyValuePair<string, Type>> map);
+        IList<TEntity> Search(int scope, string filter, IDictionary<string, KeyValuePair<string, MethodInfo>> map );
     }
 
     public class LdapServerConfiguration
@@ -130,30 +130,26 @@ namespace ASTV.Services {
 
     public class LDAPContext<TEntity> : LDAPContext, ILDAPContext<TEntity> where TEntity : class, new() 
     {
-        static public  IDictionary<string, KeyValuePair<string, Type>> DefaultMap() {            
-            Type t = typeof(TEntity);
-            Console.WriteLine(typeof(TEntity).Name);
-            var Dict = new Dictionary<string, KeyValuePair<string, Type>>();
+        static public  IDictionary<string, KeyValuePair<string, MethodInfo>> DefaultMap() {            
+            Type t = typeof(TEntity);            
+            var Dict = new Dictionary<string, KeyValuePair<string, MethodInfo>>();
             var sourceFields =  t.GetProperties();
             foreach(var field in sourceFields) {
-                var kv = new KeyValuePair<string, Type>(field.Name, field.PropertyType);                
+                var kv = new KeyValuePair<string, MethodInfo>(field.Name, null);                
                 Dict.Add(field.Name, kv);
-                Console.WriteLine("kv {0}", kv.ToString());
+                
             }
             return Dict;
         } 
-        IDictionary<string, KeyValuePair<string, Type>> _attributeMap; // attribute to query -> mapped to property of type type in class.        
+        IDictionary<string, KeyValuePair<string, MethodInfo>> _attributeMap; // attribute to query -> mapped to property of type type in class.        
         public LDAPContext(LdapServerConfiguration configuration, ILoggerFactory loggerFactory):base(configuration, loggerFactory) {
-            this._attributeMap = DefaultMap();
-            foreach(var kv in _attributeMap) {
-                Console.WriteLine("{0} => {1}:{2}", kv.Key, kv.Key, kv.Value.ToString());
-            }
+            this._attributeMap = DefaultMap();           
         }
 
         // we have POCO class. We need to map its parameters to attribs.
         // TODO: create mapping upon creating of ldap search so it is compiled and thus faster.
         // TODO: cache compiled mappings in static class cache
-        private TEntity MapLdapEntryEx(LdapEntry entry,  IDictionary<string, KeyValuePair<string, MethodInfo>> map) {
+        private TEntity MapLdapEntry(LdapEntry entry,  IDictionary<string, KeyValuePair<string, MethodInfo>> map) {
             // If method is defined, use this to convert value to required type.
             if (entry == null) return null;
             if (map == null) throw new ArgumentNullException();
@@ -190,7 +186,7 @@ namespace ASTV.Services {
                                 string[] s = attribute.StringValueArray;       
                                 p.SetValue(result, s, null);
                             } else {
-                                string Error = string.Format("Cannot convert source attribute {0} type to {1}", attribute.Name, tt.FullName); 
+                                string Error = string.Format("Cannot convert source attribute {0} type to {1}", attribute.Name, tt.FullName);                                 
                                 throw new Exception(Error);
                             }
                         } else {
@@ -198,6 +194,7 @@ namespace ASTV.Services {
                             if (!p.PropertyType.IsAssignableFrom(mi.ReturnType) ) {
                                 string Error = string.Format("Cannot invoke method with unassignable return type {0}({1}):{2} type to {3}", 
                                     mi.Name, attribute.Name, mi.ReturnType.FullName, tt.FullName); 
+                                
                                 throw new Exception(Error);
                             }
 
@@ -206,7 +203,7 @@ namespace ASTV.Services {
                             // first parameter of method must be one we can use
                             // there should be no other parameters..
                             if (mi.GetParameters().Count() != 1) {
-                                string Error = string.Format("Parameter count mismatch, expecting function with one input!"); 
+                                string Error = string.Format("Parameter count mismatch, expecting function with one input!");                                 
                                 throw new Exception(Error);
                             }
 
@@ -224,82 +221,38 @@ namespace ASTV.Services {
                                 ip[0] = attribute.ByteValueArray;
                             } else {
                                 string Error = string.Format("Unable to find suitable input parameter."); 
+                                
                                 throw new Exception(Error);
                             }
                             try {
                                 var v = mi.Invoke(null, ip);
                                 p.SetValue(result, v, null);
-                            } catch (Exception e) {
+                            } catch (Exception e) {                                
                                 throw e;
                             }
-
-
                         }
                          
                     }
                 }
 
-            }
-            Console.WriteLine(result.Serialize(null));
+            }           
             return result;
             
         }
-        private TEntity MapLdapEntry(LdapEntry entry,  IDictionary<string, KeyValuePair<string, Type>> map) {
-            
-            if (entry == null) return null;
-            if (map == null) throw new ArgumentNullException();
-
-            LdapAttributeSet attributeSet = entry.getAttributeSet();
-            IEnumerator ienum=attributeSet.GetEnumerator();
-
-            
-            // create result object
-            TEntity result = new TEntity();
-
-            var destProps = typeof(TEntity).GetProperties()
-                    .Where(x => x.CanWrite)
-                    .ToList();
-
-            // iterate trhough attributes..
-            while(ienum.MoveNext()) {
-                LdapAttribute attribute=(LdapAttribute)ienum.Current;
-                string attributeName = attribute.Name;
-                               
-                // look up if we have this mapped
-                if (map.ContainsKey(attributeName)) {
-                    var kv = map[attributeName];
-                    if (destProps.Any(x => x.Name == kv.Key)) {
-                        var p = destProps.First(x => x.Name == kv.Key);
-                        
-                        if (kv.Value == typeof(System.String)) {
-                            string s = attribute.StringValue;       
-                            p.SetValue(result, s, null);                
-                        } else if (kv.Value == typeof(System.String[])) {
-                            String[] s =   attribute.StringValueArray;
-                            p.SetValue(result, s, null);
-                        } 
-                    }
-                }
-
-            }
-            Console.WriteLine(result.Serialize(null));
-            return result;
-        }
+       
 
         public virtual IList<TEntity> Search(int scope, string filter ) {            
             return Search(scope, filter, this._attributeMap);
         }
-
-
-        public virtual IList<TEntity> Search(int scope, string filter, IDictionary<string, KeyValuePair<string, Type>> map ) {
+       
+         public virtual IList<TEntity> Search(int scope, string filter, IDictionary<string, KeyValuePair<string, MethodInfo>> map ) {
 
             if (map == null) throw new ArgumentNullException();
 
             IList<TEntity> results = new List<TEntity>();
             
             // basic search
-            string[] attrs = map.Keys.ToArray();
-            Console.WriteLine("ATTRS {0}", attrs.Serialize(null));
+            string[] attrs = map.Keys.ToArray();            
 
             using (var ldapConnection = new LdapConnection()) {
                  try {
@@ -337,69 +290,6 @@ namespace ASTV.Services {
                         
                         try {
                             TEntity result = MapLdapEntry(nextEntry, map);
-                            results.Add(result);                                
-                        } catch ( Exception e) {
-                            _logger.LogError(e.Message + ": "+ e.StackTrace);
-                            throw e;
-                        }
-                    
-                     }
-
-                     ldapConnection.Disconnect();
-                } catch (Exception e) {
-                    _logger.LogError(e.StackTrace);                    
-                }
-            }               
-            return results;
-        }
-
-
-         public virtual IList<TEntity> Search(int scope, string filter, IDictionary<string, KeyValuePair<string, MethodInfo>> map ) {
-
-            if (map == null) throw new ArgumentNullException();
-
-            IList<TEntity> results = new List<TEntity>();
-            
-            // basic search
-            string[] attrs = map.Keys.ToArray();
-            Console.WriteLine("ATTRS {0}", attrs.Serialize(null));
-
-            using (var ldapConnection = new LdapConnection()) {
-                 try {
-                     LdapConstraints lr = new LdapConstraints();
-                     
-                     ldapConnection.Connect(_configuration.ServerAddress, _configuration.ServerPort);
-                     ldapConnection.Bind(_configuration.RootUserDn, _configuration.RootUserPassword);
-                     
-                     // search.                     
-                     LdapSearchConstraints lsconstr = new LdapSearchConstraints();
-                     lsconstr.MaxResults = 1000; // Actual limit imposed by LDAP server.  
-                     
-                     LdapSearchResults lsc=ldapConnection.Search(	_configuration.SearchBase,
-												scope,
-												filter,
-												attrs,
-												false,
-                                                lsconstr);
-                    
-                     while (lsc.hasMore()) {
-                        LdapEntry nextEntry = null;
-                        
-                        try 
-                        {
-                            nextEntry = lsc.next();
-                        }
-                        catch(LdapException e1) 
-                        {                         
-                            // If limit is exceeded then should get last value and start search from next value. However, its too complicated to solve now..   
-                            _logger.LogError(e1.Message + ": "+ e1.StackTrace);
-                            
-                            // Exception is thrown, go for next entry
-                            continue;
-                        }
-                        
-                        try {
-                            TEntity result = MapLdapEntryEx(nextEntry, map);
                             results.Add(result);                                
                         } catch ( Exception e) {
                             _logger.LogError(e.Message + ": "+ e.StackTrace);
